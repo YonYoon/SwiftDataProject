0f5a6999df221a38c3338b639f89c55139ac6a23
YonYoon <zhzhzh1806@gmail.com>
2024-01-17 14:30:56 +0500

    Created model container

--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Zhansen Zhalel on 17.01.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
