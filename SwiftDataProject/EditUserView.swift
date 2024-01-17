fb8a72cb5065bf355eef9decf36dbd0496ea7bff
YonYoon <zhzhzh1806@gmail.com>
2024-01-17 14:31:32 +0500

    Created EditUser view

--- SwiftDataProject/EditUserView.swift
+++ SwiftDataProject/EditUserView.swift
@@ -5,14 +5,31 @@
 //  Created by Zhansen Zhalel on 17.01.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct EditUserView: View {
+    @Bindable var user: User
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Name", text: $user.name)
+            TextField("City", text: $user.city)
+            DatePicker("Join Date", selection: $user.joinDate)
+        }
+        .navigationTitle("Edit User")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    EditUserView()
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = ModelContainer(for: User.self, configurations: config)
+        let user = User(name: "John", city: "Aktobe", joinDate: .now)
+        return EditUserView(user: user)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create container: \(error.localizedDescription)")
+    }
 }
