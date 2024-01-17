3338aab26d8ca364f7d45423608faa41c33d164d
YonYoon <zhzhzh1806@gmail.com>
2024-01-17 14:29:54 +0500

    Created model and user class

---
+++ SwiftDataProject/EditUserView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUserView.swift
+//  SwiftDataProject
+//
+//  Created by Zhansen Zhalel on 17.01.2024.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserView()
+}
