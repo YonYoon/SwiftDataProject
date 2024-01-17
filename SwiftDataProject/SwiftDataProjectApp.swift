c694ef68b925cebeb9b7bc5e99e91d7adf977c84
YonYoon <zhzhzh1806@gmail.com>
2024-01-17 14:01:04 +0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Zhansen Zhalel on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
